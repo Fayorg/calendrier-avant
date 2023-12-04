e844983b41cd2c943cd423d2e52a0255d7561eb7
Fayorg <ebr@aketechnology.com>
2023-12-04 10:49:46 +0100

    fix: api route

--- app/api/grade/route.ts
+++ app/api/grade/route.ts
@@ -11,13 +11,12 @@ export async function POST(req: Request){
     await prisma.grade.findFirst({where: {key: body.key}}).then(async (grade) => {
         if (grade) {
             await prisma.grade.create({data: {test: 1, grade: body.grade, key: body.key}})
-            return new NextResponse(
-                JSON.stringify({message: 'Grade updated', cookie: 'voted=true'}), {status: 200}
-            )
+            return NextResponse.json({message: 'Grade updated', cookie: 'voted=true'}, {status: 200})
         } else {
-            return new NextResponse(
+            return NextResponse.json(
                 JSON.stringify({message: 'Key not found'}), {status: 404}
             )
         }
-    })
+    });
+    return NextResponse.json({message: 'Server error'}, {status: 500})
 }
