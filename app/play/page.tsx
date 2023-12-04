d21fa01ff230fffef40362aba98b98ecefea7d34
Fayorg <ebr@aketechnology.com>
2023-12-04 20:24:39 +0100

    fix: linting problems

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -3,19 +3,17 @@
 import { useRouter } from 'next/navigation';
 import { useState, useEffect } from 'react';
 
-export default function play() {
+export default function Play() {
 	const [grade, setGrade] = useState<number>(0);
 	const router = useRouter();
-	let voted;
 	const [password, setPassword] = useState<string | null>('');
 	useEffect(() => {
 		const pass = localStorage.getItem('@password');
 		setPassword(pass);
-		voted = localStorage.getItem('@voted');
 		if (!pass) {
 			router.push('/');
 		}
-	}, []);
+	}, [router]);
 
 	async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
 		event.preventDefault();
@@ -35,50 +33,45 @@ export default function play() {
 	return (
 		<div>
 			<h1>Calendrier-avant</h1>
-
-			{voted ? (
-				<p>Vous avez déjà voté</p>
-			) : (
-				<div>
-					<p>Cliquer sur la note à la fin de la présentation :</p>
-					<form onSubmit={handleSubmit}>
-						<input type="radio" id="one" name="grade" checked={grade == 1} onChange={(e) => setGrade(1)}></input>
-						<label htmlFor="one">1</label>
-						<br></br>
-						<input type="radio" id="15" name="grade" checked={grade == 1.5} onChange={(e) => setGrade(1.5)}></input>
-						<label htmlFor="one">1.5</label>
-						<br></br>
-						<input type="radio" id="2" name="grade" checked={grade == 2} onChange={(e) => setGrade(2)}></input>
-						<label htmlFor="one">2</label>
-						<br></br>
-						<input type="radio" id="25" name="grade" checked={grade == 2.5} onChange={(e) => setGrade(2.5)}></input>
-						<label htmlFor="one">2.5</label>
-						<br></br>
-						<input type="radio" id="3" name="grade" checked={grade == 3} onChange={(e) => setGrade(3)}></input>
-						<label htmlFor="one">3</label>
-						<br></br>
-						<input type="radio" id="35" name="grade" checked={grade == 3.5} onChange={(e) => setGrade(3.5)}></input>
-						<label htmlFor="one">3.5</label>
-						<br></br>
-						<input type="radio" id="4" name="grade" checked={grade == 4} onChange={(e) => setGrade(4)}></input>
-						<label htmlFor="one">4</label>
-						<br></br>
-						<input type="radio" id="45" name="grade" checked={grade == 4.5} onChange={(e) => setGrade(4.5)}></input>
-						<label htmlFor="one">4.5</label>
-						<br></br>
-						<input type="radio" id="5" name="grade" checked={grade == 5} onChange={(e) => setGrade(5)}></input>
-						<label htmlFor="one">5</label>
-						<br></br>
-						<input type="radio" id="55" name="grade" checked={grade == 5.5} onChange={(e) => setGrade(5.5)}></input>
-						<label htmlFor="one">5.5</label>
-						<br></br>
-						<input type="radio" id="6" name="grade" checked={grade == 6} onChange={(e) => setGrade(6)}></input>
-						<label htmlFor="one">6</label>
-						<br></br>
-						<input type="submit" value={'Envoyer'}></input>
-					</form>
-				</div>
-			)}
+			<div>
+				<p>Cliquer sur la note à la fin de la présentation :</p>
+				<form onSubmit={handleSubmit}>
+					<input type="radio" id="one" name="grade" checked={grade == 1} onChange={(e) => setGrade(1)}></input>
+					<label htmlFor="one">1</label>
+					<br></br>
+					<input type="radio" id="15" name="grade" checked={grade == 1.5} onChange={(e) => setGrade(1.5)}></input>
+					<label htmlFor="one">1.5</label>
+					<br></br>
+					<input type="radio" id="2" name="grade" checked={grade == 2} onChange={(e) => setGrade(2)}></input>
+					<label htmlFor="one">2</label>
+					<br></br>
+					<input type="radio" id="25" name="grade" checked={grade == 2.5} onChange={(e) => setGrade(2.5)}></input>
+					<label htmlFor="one">2.5</label>
+					<br></br>
+					<input type="radio" id="3" name="grade" checked={grade == 3} onChange={(e) => setGrade(3)}></input>
+					<label htmlFor="one">3</label>
+					<br></br>
+					<input type="radio" id="35" name="grade" checked={grade == 3.5} onChange={(e) => setGrade(3.5)}></input>
+					<label htmlFor="one">3.5</label>
+					<br></br>
+					<input type="radio" id="4" name="grade" checked={grade == 4} onChange={(e) => setGrade(4)}></input>
+					<label htmlFor="one">4</label>
+					<br></br>
+					<input type="radio" id="45" name="grade" checked={grade == 4.5} onChange={(e) => setGrade(4.5)}></input>
+					<label htmlFor="one">4.5</label>
+					<br></br>
+					<input type="radio" id="5" name="grade" checked={grade == 5} onChange={(e) => setGrade(5)}></input>
+					<label htmlFor="one">5</label>
+					<br></br>
+					<input type="radio" id="55" name="grade" checked={grade == 5.5} onChange={(e) => setGrade(5.5)}></input>
+					<label htmlFor="one">5.5</label>
+					<br></br>
+					<input type="radio" id="6" name="grade" checked={grade == 6} onChange={(e) => setGrade(6)}></input>
+					<label htmlFor="one">6</label>
+					<br></br>
+					<input type="submit" value={'Envoyer'}></input>
+				</form>
+			</div>
 		</div>
 	);
 }
