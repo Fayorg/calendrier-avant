fcdcc14b85d9e58bbd77e12a42a5afe3e074daaa
Fayorg <ebr@aketechnology.com>
2023-12-05 23:52:41 +0100

    fix: created components for voting page

---
+++ components/custom/index.ts
@@ -0,0 +1,3 @@
+export { DayCard as DayCard } from './dayCard';
+export { TestCard as TestCard } from './testCard';
+export { GradingForm } from "./gradingForm";
---
+++ components/custom/testCard.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+export function TestCard({ data, error, isLoading }: { data: any; error: any; isLoading: boolean }) {
+	if (isLoading)
+		return (
+			<div className="rounded-lg border-gray-700 border-2">
+				<h2>Aujourd'hui</h2>
+				<h3>Chargement...</h3>
+			</div>
+		);
+	if (error)
+		return (
+			<div className="rounded-lg border-gray-700 border-2">
+				<h2>Aujourd'hui</h2>
+				<h3>Une erreur est survenue, merci d'essayer dans quelques minutes</h3>
+			</div>
+		);
+	if (data.status == 404)
+		return (
+			<div className="rounded-lg border-gray-700 border-2">
+				<h2>{"Aujourd'hui " + new Date().toLocaleDateString()}</h2>
+				<h3>Pas de test prévu pour aujourd'hui</h3>
+			</div>
+		);
+
+	return (
+		<div className="rounded-lg border-gray-700 border-2">
+			<h2>{"Aujourd'hui " + new Date(data.testOn).toLocaleDateString()}</h2>
+			<h3>{data.testOf.firstName + ' ' + data.testOf.lastName}</h3>
+		</div>
+	);
+}
