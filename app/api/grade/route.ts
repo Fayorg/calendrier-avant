2e141a19ce4c83c2a4502dca8fbfec1fd622c1f3
Fayorg <ebr@aketechnology.com>
2023-12-05 21:37:47 +0100

    fix: removed api

--- app/api/grade/route.ts
+++ app/api/grade/route.ts
@@ -7,12 +7,5 @@ interface IBody {
 }
 
 export async function POST(req: Request){
-    const body = await req.json() as IBody
-    const grade = await prisma.grade.findFirst({ select: { id: true}, where: { oral: 1, key: { key: body.key } } });
-    if(grade) return NextResponse.json({message: 'Deja vote'}, {status: 403});
-
-    const gradeCreated = await prisma.grade.create({ data: { key: { connect: { key: body.key } }, grade: body.grade, oral: 1 } });
-    if(gradeCreated) return NextResponse.json({message: 'Vote enregistré'}, {status: 200});
-
     return NextResponse.json({message: 'Server error'}, {status: 500})
 }
