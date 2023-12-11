03e91d766effadd92b3f6bf3b806975f477726c3
timhaller <67918170+timhaller@users.noreply.github.com>
2023-12-11 13:28:30 +0100

    fix to style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,19 @@
-import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '@styles/global.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: 'Calendrier de l\'avent',
+	description: 'Calendrier de l\'avent de maths de la 3M03',
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
+		<head><title></title><meta name="viewport" content="viewport-fit=cover" /></head>
 			<body className={inter.className}>{children}</body>
 		</html>
 	);
