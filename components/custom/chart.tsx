47e1204be791cc4d4bc58603ac8c01bcab26c053
timhaller <67918170+timhaller@users.noreply.github.com>
2023-12-15 14:51:28 +0100

    fix to style on chart.tsx

--- components/custom/chart.tsx
+++ components/custom/chart.tsx
@@ -11,19 +11,21 @@ interface ChartProps {
 
 export function Chart({...props}: ChartProps) {
     return (
-        <div className={"text-[#F0F0F0]"}>
-            <div className={"flex flex-row justify-between w-full items-end"}>
-                {props.data.map((item, index) => (
-                    <Santa height={item.total} key={index}/>
-                ))}
-            </div>
-            <div className={"flex flex-row justify-between w-full items-end mt-2"}>
-                {props.data.map((item, index) => (
-                    <div className={"w-[100px] h-fit flex flex-col items-center"} key={index}>
-                        <div className={"h-[14px] w-[3pt] bg-white"}/>
-                        <span className={""}>{item.name}</span>
-                    </div>
-                ))}
+        <div className={"text-[#F0F0F0] flex-1 flex items-end"}>
+            <div className={"w-full"}>
+                <div className={"flex flex-row justify-between w-full items-end"}>
+                    {props.data.map((item, index) => (
+                        <Santa height={item.total} key={index}/>
+                    ))}
+                </div>
+                <div className={"flex flex-row justify-between w-full items-end mt-2"}>
+                    {props.data.map((item, index) => (
+                        <div className={"w-[100px] h-fit flex flex-col items-center"} key={index}>
+                            <div className={"h-[14px] w-[3pt] bg-white"}/>
+                            <span className={""}>{item.name}</span>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     )
